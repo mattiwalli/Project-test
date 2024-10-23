98a7b2f786be7d8a7dbae2cd8a17af26cc339e98
mattiwalli <mattiwalli_2000@hotmail.com>
2024-10-23 09:12:39 +0200

    k

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const menuE1 = document.querySelector(".mobile-menu-button button")
     menuE1.addEventListener('click', ()=>{
         const mobilMenuWrapper = document.querySelector('.mobile-menu-wrapper');
         mobilMenuWrapper.classList.toggle('menu-open');
-        
-
-        console.log('click');
-        
+            
         
     });
 
